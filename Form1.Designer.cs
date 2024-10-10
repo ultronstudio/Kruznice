e78819f2f76fe5b0fd2030d61b2d465b382c7b5e
Petr Vurm <p.vurm@outlook.cz>
2024-10-10 10:13:54 +0200

    Přidejte soubory projektu.

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Kruznice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCircleCount = new System.Windows.Forms.TextBox();
+            this.lblCircleCount = new System.Windows.Forms.Label();
+            this.txtMinRadius = new System.Windows.Forms.TextBox();
+            this.lblMinRadius = new System.Windows.Forms.Label();
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtCircleCount
+            // 
+            this.txtCircleCount.Location = new System.Drawing.Point(12, 26);
+            this.txtCircleCount.Name = "txtCircleCount";
+            this.txtCircleCount.Size = new System.Drawing.Size(100, 20);
+            this.txtCircleCount.TabIndex = 0;
+            // 
+            // lblCircleCount
+            // 
+            this.lblCircleCount.AutoSize = true;
+            this.lblCircleCount.Location = new System.Drawing.Point(12, 9);
+            this.lblCircleCount.Name = "lblCircleCount";
+            this.lblCircleCount.Size = new System.Drawing.Size(72, 13);
+            this.lblCircleCount.TabIndex = 1;
+            this.lblCircleCount.Text = "Počet kružnic";
+            // 
+            // txtMinRadius
+            // 
+            this.txtMinRadius.Location = new System.Drawing.Point(118, 26);
+            this.txtMinRadius.Name = "txtMinRadius";
+            this.txtMinRadius.Size = new System.Drawing.Size(100, 20);
+            this.txtMinRadius.TabIndex = 2;
+            // 
+            // lblMinRadius
+            // 
+            this.lblMinRadius.AutoSize = true;
+            this.lblMinRadius.Location = new System.Drawing.Point(118, 10);
+            this.lblMinRadius.Name = "lblMinRadius";
+            this.lblMinRadius.Size = new System.Drawing.Size(92, 13);
+            this.lblMinRadius.TabIndex = 3;
+            this.lblMinRadius.Text = "Minimální poloměr";
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.Location = new System.Drawing.Point(12, 52);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(75, 23);
+            this.btnDraw.TabIndex = 4;
+            this.btnDraw.Text = "Vykreslit";
+            this.btnDraw.UseVisualStyleBackColor = true;
+            this.btnDraw.Click += new System.EventHandler(this.btnDraw_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(713, 12);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 5;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(632, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 6;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkOrange;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnDraw);
+            this.Controls.Add(this.lblMinRadius);
+            this.Controls.Add(this.txtMinRadius);
+            this.Controls.Add(this.lblCircleCount);
+            this.Controls.Add(this.txtCircleCount);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.HelpButton = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kruznice";
+            this.HelpButtonClicked += new System.ComponentModel.CancelEventHandler(this.Form1_HelpButtonClicked);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCircleCount;
+        private System.Windows.Forms.Label lblCircleCount;
+        private System.Windows.Forms.TextBox txtMinRadius;
+        private System.Windows.Forms.Label lblMinRadius;
+        private System.Windows.Forms.Button btnDraw;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Kruznice
+{
+    public partial class Form1 : Form
+    {
+        private int circleCount;
+        private int minRadius;
+        private Timer animationTimer;
+        private int animationStep = 0;
+        private bool isGrowing = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Inicializace časovače
+            animationTimer = new Timer();
+            animationTimer.Interval = 50; // Nastavení intervalu v milisekundách (rychlost animace)
+            animationTimer.Tick += AnimationTimer_Tick;
+        }
+
+        private void btnDraw_Click(object sender, EventArgs e)
+        {
+            // Načtěte hodnoty z textových polí
+            if (int.TryParse(txtCircleCount.Text, out circleCount) && int.TryParse(txtMinRadius.Text, out minRadius))
+            {
+                this.Invalidate(); // Překreslí okno
+            }
+            else
+            {
+                MessageBox.Show("Zadejte platná čísla.");
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            animationStep = 0; // Resetuje animaci
+            isGrowing = true;
+            animationTimer.Start(); // Spustí časovač
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            animationTimer.Stop(); // Zastaví časovač
+            this.Invalidate(); // Překreslí okno, aby se animace zastavila
+        }
+
+        private void AnimationTimer_Tick(object sender, EventArgs e)
+        {
+            // Zvětšení nebo zmenšení radiusu
+            if (isGrowing)
+            {
+                animationStep += 2; // Přidá k radiusu
+                if (animationStep > 50) isGrowing = false; // Když dosáhne určité velikosti, začne se zmenšovat
+            }
+            else
+            {
+                animationStep -= 2; // Zmenší radius
+                if (animationStep < -50) isGrowing = true; // Když je moc malý, začne se zvětšovat
+            }
+
+            this.Invalidate(); // Překreslí formulář s novým nastavením
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            if (circleCount > 0 && minRadius > 0)
+            {
+                Graphics g = e.Graphics;
+                int width = this.ClientSize.Width;
+                int height = this.ClientSize.Height;
+
+                // Určíme střed
+                Point center = new Point(width / 2, height / 2);
+                int radiusStep = 10; // Krok mezi kružnicemi
+
+                for (int i = 0; i < circleCount; i++)
+                {
+                    // Zvýšení/zmenšení velikosti podle animationStep
+                    int radius = minRadius + i * radiusStep + animationStep;
+                    g.DrawEllipse(Pens.Black, center.X - radius, center.Y - radius, radius * 2, radius * 2);
+                }
+            }
+        }
+
+        private void Form1_HelpButtonClicked(object sender, CancelEventArgs e)
+        {
+            MessageBox.Show($"Aplikace Kruznice\n2024 © Petr Vurm\n\nVytvořeno jako práce v hodině z předmětu PROGRAMOVÁNÍ na SPŠ Hradební.", "O aplikaci", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
